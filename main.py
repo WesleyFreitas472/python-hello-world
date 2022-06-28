94aad19f03add2c4c22f6278af4ca2f37a5300c3
WesleyFreitas472 <41868116+WesleyFreitas472@users.noreply.github.com>
2022-06-28 14:42:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask! Hello World'
+    return os.getenv("MESSAGE", 'Web App with Python Flask! Hello World')
 
 app.run(host='0.0.0.0', port=8080)
